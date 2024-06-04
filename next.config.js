402cbff62552de7e9b3c7644861a4abfc73c3558
Cameron Lai <cameronlai@users.noreply.github.com>
2024-06-04 15:23:37 +0800

    add new code

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/tailwind-landing-page-template-v2',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
